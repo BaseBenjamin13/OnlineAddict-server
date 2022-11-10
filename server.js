e24eed6737e16d24c8f2adf918e9bc5d8a3b0770
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-11-10 13:40:04 -0500

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+
+//CONTROLLERS
+
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/favicon.ico', (req, res) => {
+    // console.log('favicon')
+})
+
+
+const port = process.env.PORT || 5005;
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
